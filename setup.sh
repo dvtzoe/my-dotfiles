91a5770bc6cead724ede4928b096b69c1df784fd
Ohm_2018 <102822222+DVTZOE@users.noreply.github.com>
2024-11-08 19:05:00 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-cd ~
 mv ./assets ~
 mv ./ags ~/.config
 rm ./hypr/hyprland.conf
@@ -6,31 +5,29 @@ mv ./hypr ~/.config
 mv ./kitty ~/.config
 mv ./.bashrc ~
 
-mkdir git
-cd git
-
+cd ~
 sudo pacman -S --needed git base-devel hyprland kitty dolphin firefox bluez bluez-utils brightnessctl playerctl grim slurp wl-utils rofi rust
+mkdir git
 
+cd git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 yay -S --needed aylurs-gtk-shell
 
 cd ~/git
-
 git clone https://github.com/adi1090x/rofi.git
 cd rofi
 chmod +x setup.sh
 ./setup.sh
-cd ~/git
 
+cd ~/git
 git clone https://github.com/LGFae/swww.git
 cd swww
 cargo build --release
 sudo mv target/release/swww target/release/swww-daemon /bin
 
 cd ~/git
-
 git clone https://github.com/ful1e5/Bibata_Cursor.git
 
 echo 'alr should be done'
