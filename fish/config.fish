73c84320b60ec293a6a4d869eb92a4f2f70cb7b9
Ohm_2018 <102822222+DVTZOE@users.noreply.github.com>
2024-11-19 20:24:02 +0700

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias apcman=pacman
+alias pcaman=pacman
+alias pamcan=pacman
+alias pacamn=pacman
+alias pacmna=pacman
+
+alias kys=poweroff
